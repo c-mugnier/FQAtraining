d2e945372589a08875601db4cda5fcbf142c5d59
c-mugnier <mugnier.charles@gmail.com>
2014-09-10 11:40:23 +0700

    test project

--- MyProject/src/MyPackage/Calendar.java
+++ MyProject/src/MyPackage/Calendar.java
@@ -6,44 +6,63 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class Calendar {
-	public static void main(String[] args) {
+public class Calendar extends Main {
+	public Calendar() {
 		
 		
 	}
 	
-	public static void open(WebDriver driver) {
+	public void open(WebDriver driver) {
 		//Click on the calendar button
-		driver.findElement(By.xpath("//*[@data-original-title='Calendar']")).click();
+		driver.findElement(ELEMENT_HOMEPAGE_SIDEBOX_CALENDARBUTTON).click();
 	}
 	
 	
-	public static void test1(WebDriver driver, String eventTest) {
+	public void test1(WebDriver driver, String eventTest) {
 		//Click on the add quick event button
-		driver.findElement(By.id("UIActionBarQuickAddEvent")).click();
-		WebElement eventTitle =driver.findElement(By.id("eventName"));
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENTBUTTON).click();
+		//Set the event name
+		WebElement eventTitle =driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENT_NAMEFIELD);
 		eventTitle.sendKeys(eventTest);
+		//wait
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		driver.findElement(By.xpath("html/body/div[1]/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/table/tbody/tr/td[2]/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div[8]/div[2]/div[3]/div[2]/div/div[2]/button[1]")).click();
+		//Click on valid button
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENT_SAVEBUTTON).click();
 	}
 	
 	
-	public static void test2(WebDriver driver, String eventTest2) {
+	public void test2(WebDriver driver, String eventTest2) {
 		
-		driver.findElement(By.id("UIActionBarQuickAddEvent")).click();
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENTBUTTON).click();
 		WebElement eventTitle =driver.findElement(By.id("eventName"));
 		eventTitle.sendKeys(eventTest2);
+		//Display all the tabs
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//*[@id='UIQuickAddEventPopupWindow']//*[text()='More Details']")).click();
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS).click();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//*[@data-toggle='tab' and text()='Reminders']")).click();
+		driver.findElement (ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS_REMINDERS).click();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//*[@data-toggle='tab' and text()='Participants']")).click();
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS_PARTICIPANTS).click();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//*[@data-toggle='tab' and text()='Schedule']")).click();
-		driver.findElement(By.xpath("html/body/div[1]/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/table/tbody/tr/td[2]/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/span/div/div[2]/div/div/form/div[4]/button[1]")).click();
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS_SCHEDULE).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS_SAVEBUTTON).click();
 	
 	
 	}
+	
+	public void test3(WebDriver driver, String eventTest3) {
+		//Click on the add quick event button
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENTBUTTON).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement eventTitle =driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENT_NAMEFIELD);
+		eventTitle.sendKeys(eventTest3);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENT_ALLDAYCHECKBOX).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		
+		driver.findElement(ELEMENT_CALENDAR_QUICKADDEVENT_SAVEBUTTON).click();
+	}
+	
 }
--- MyProject/src/MyPackage/Main.java
+++ MyProject/src/MyPackage/Main.java
@@ -7,6 +7,25 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class Main {
+	
+	// declaration of the elements
+	// homepage button
+	public final By ELEMENT_HOMEPAGE_SIDEBOX_CALENDARBUTTON =  By.xpath("//*[@data-original-title='Calendar']");
+	
+	// calendar page button
+	public final By ELEMENT_CALENDAR_QUICKADDEVENTBUTTON = By.id("UIActionBarQuickAddEvent");
+	public final By ELEMENT_CALENDAR_QUICKADDEVENT_NAMEFIELD = By.id("eventName");
+	public final By ELEMENT_CALENDAR_QUICKADDEVENT_SAVEBUTTON =  By.xpath ("//*[@id='UIQuickAddEventPopupWindow']//*[text()='Save']");
+	public final By ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS =  By.xpath ("//*[@id='UIQuickAddEventPopupWindow']//*[text()='More Details']");
+	public final By ELEMENT_CALENDAR_QUICKADDEVENT_ALLDAYCHECKBOX = By.xpath("//*[@id='UIQuickAddEvent']//*[@name = 'allDay']");
+	public final By ELEMENT_CALENDAR_QUICKADDEVENT_CANCEL =  By.xpath ("//*[@id='UIQuickAddEventPopupWindow']//*[text()='Cancel']");
+	
+	public final By ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS_REMINDERS =  By.xpath ("//*[@data-toggle='tab' and text()='Reminders']");
+	public final By ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS_PARTICIPANTS =  By.xpath ("//*[@data-toggle='tab' and text()='Participants']");
+	public final By ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS_SCHEDULE =  By.xpath ("//*[@data-toggle='tab' and text()='Schedule']");
+	public final By ELEMENT_CALENDAR_QUICKADDEVENT_MOREDETAILS_SAVEBUTTON = By .xpath("//*[@id='UIEventForm']//*[text()='Save']");
+	
+	
 	public static void main(String[] args) throws InterruptedException {
 		
         // declaration and instantiation of objects/variables
@@ -15,16 +34,20 @@ public class Main {
 		String username = "root";
 		String password = "gtn";
 		String eventTest = "event test1";
-		String eventTest2 = "event test2";
+		String eventTest2 = "event <test2>";
+		String eventTest3 = "event test3";
+		Calendar calendar= new Calendar();
+		
 		
 		Log.main(driver, baseUrl, username, password);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		Calendar.open(driver);
+		calendar.open(driver);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		Calendar.test1(driver, eventTest);
+		calendar.test1(driver, eventTest);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		Calendar.test2(driver, eventTest2);
-		
+		calendar.test2(driver, eventTest2);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		calendar.test3(driver, eventTest3);
 		
 		
 	  }
